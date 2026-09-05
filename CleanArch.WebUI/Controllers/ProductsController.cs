b40a8f5368f5c8596dda1b25834418707cadf7ad
Cristian Tavares <cristianray.c@gmail.com>
2026-09-05 19:28:01 -0300

    feat(webui): add controllers and Razor views for Products and Categories

--- CleanArch.WebUI/Controllers/ProductsController.cs
+++ CleanArch.WebUI/Controllers/ProductsController.cs
@@ -1,15 +1,23 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArch.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productAppService,
+            ICategoryService categoryService, IWebHostEnvironment environment)
         {
-            _productService = productService;
+            _productService = productAppService;
+            _categoryService = categoryService;
+            _environment = environment;
         }
 
         [HttpGet]
@@ -18,5 +26,85 @@ namespace CleanArch.WebUI.Controllers
             var products = await _productService.GetProductsAsync();
             return View(products);
         }
+
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId =
+            new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDto = await _productService.GetByIdAsync(id);
+
+            if (productDto == null) return NotFound();
+
+            var categories = await _categoryService.GetCategoriesAsync();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.UpdateAsync(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var productDto = await _productService.GetByIdAsync(id);
+
+            if (productDto == null) return NotFound();
+
+            return View(productDto);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _productService.RemoveAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDto = await _productService.GetByIdAsync(id);
+
+            if (productDto == null) return NotFound();
+            var wwwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + productDto.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
+
+            return View(productDto);
+        }
+
     }
 }
