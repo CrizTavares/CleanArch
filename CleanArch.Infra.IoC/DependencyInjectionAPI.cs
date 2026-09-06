ed4569da4b1e2687e327b25d6cf61918e5d4b87c
Cristian Tavares <cristianray.c@gmail.com>
2026-09-06 20:14:16 -0300

    feat(api): add Products and Categories API controllers

--- CleanArch.Infra.IoC/DependencyInjectionAPI.cs
+++ CleanArch.Infra.IoC/DependencyInjectionAPI.cs
@@ -27,6 +27,7 @@ namespace CleanArch.Infra.IoC
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
 
             services.AddScoped<IAuthenticate, AuthenticateService>();
