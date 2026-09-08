bf9d89df915fecd1388c849d29ee68a1dfec2ef3
Cristian Tavares <cristianray.c@gmail.com>
2026-09-08 14:57:40 -0300

    feat(api): add auth models and JWT configuration settings

---
+++ CleanArch.API/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArch.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = String.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = String.Empty;
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password do not match.")]
+        public string ConfirmPassword { get; set; } = String.Empty;
+    }
+}
